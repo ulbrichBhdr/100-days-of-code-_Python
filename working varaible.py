3ddcf46b2650e908128541fefe040261c4e678a5
ulbrichBhdr <109100201+ulbrichBhdr@users.noreply.github.com>
2024-03-16 20:49:00 +0330

    day_1

---
+++ working varaible.py
@@ -0,0 +1,31 @@
+# exercise1_day_1
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this: ")
+# print("print('what to print')")
+
+# print("Hello world!\nHello world!")
+# print("Hello"+" "+"Angela")
+
+# exercise 2
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done withe the '+' sign.")
+# print('e.g. print("Hello " + "world")')
+# print("(New lines can be created with a backslash and n.")
+
+# print("Hello " + input("what is your name ?"))
+
+#exercise 3
+# Name = input("What is your name ?")
+# number_chr = int(len(Name))
+# print(number_chr)
+
+#exercise 4
+a = input("a:")
+b = input("b:")
+
+c = a 
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
