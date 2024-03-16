dc08f21c6ad63fb6b086c18b0f9e92148541c008
ulbrichBhdr <109100201+ulbrichBhdr@users.noreply.github.com>
2024-03-16 21:13:02 +0330

    Add files via upload

---
+++ day1/finalProject_day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+Name1 = input("Which city did you grow up in?\n")
+Name2 = input("what is name of pet?\n")
+print("your rockband is " + Name1 + " " + Name2 + ".")
