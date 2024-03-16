ca0ed123e6073db7edec71597b6aff4c9540f990
ulbrichBhdr <109100201+ulbrichBhdr@users.noreply.github.com>
2024-03-16 20:53:43 +0330

    Update finalProject_day1.py

--- finalProject_day1.py
+++ finalProject_day1.py
@@ -1,4 +1,1 @@
-print("Welcome to the Band Name Generator")
-Name1 = input("Which city did you grow up in?\n")
-Name2 = input("what is name of pet?\n")
-print("your rockband is " + Name1 + " " + Name2 + ".")
+
